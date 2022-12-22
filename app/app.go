c33516ff951d866d1cfa4e30f4d5b6682eae3701
Jensen Holm <96762808+Jensen-holm@users.noreply.github.com>
2022-12-22 18:01:46 -0500

    blank canvas without using the wrapper

--- app/app.go
+++ app/app.go
@@ -4,8 +4,15 @@ import (
 	"github.com/g3n/engine/app"
 	"github.com/g3n/engine/camera"
 	"github.com/g3n/engine/core"
+	"github.com/g3n/engine/geometry"
 	"github.com/g3n/engine/gls"
+	"github.com/g3n/engine/graphic"
+	"github.com/g3n/engine/gui"
+	"github.com/g3n/engine/light"
+	"github.com/g3n/engine/material"
+	"github.com/g3n/engine/math32"
 	"github.com/g3n/engine/renderer"
+	"github.com/g3n/engine/util/helper"
 	"time"
 )
 
@@ -15,6 +22,7 @@ type App struct {
 	self  *app.Application
 	scene *core.Node
 	cam   *camera.Camera
+	help  bool
 }
 
 func NewApp() *App {
@@ -22,6 +30,7 @@ func NewApp() *App {
 		self:  app.App(),
 		scene: core.NewNode(),
 		cam:   camera.New(1),
+		help:  true,
 	}
 }
 
@@ -35,6 +44,51 @@ func (a *App) Scene() *core.Node {
 
 func (a *App) Self() *app.Application {
 	return a.self
+}
+
+func (a *App) ToggleHelp(h bool) {
+	a.help = h
+}
+
+func (a *App) AddLight(
+	posX,
+	posY,
+	posZ float32,
+	ambColor,
+	ptColor *math32.Color,
+	ambIntensity,
+	ptIntensity float32) {
+
+	// not sure if this ambient thing is good in this func
+	a.scene.Add(light.NewAmbient(
+		ambColor, ambIntensity,
+	))
+	ptLight := light.NewPoint(
+		ptColor, ptIntensity,
+	)
+	ptLight.SetPosition(posX, posY, posZ)
+
+	a.scene.Add(ptLight)
+}
+
+func (a *App) SetBG(R, G, B, A float32) {
+	a.self.Gls().ClearColor(R, G, B, A)
+}
+
+// Prep -> Must be run right before RunApp
+func (a *App) Prep() {
+	camera.NewOrbitControl(a.cam)
+	gui.Manager().Set(a.scene)
+
+	if a.help {
+		a.scene.Add(helper.NewAxes(0.5))
+	}
+
+	// Create a blue torus and add it to the scene
+	geom := geometry.NewTorus(1, .4, 12, 32, math32.Pi*2)
+	mat := material.NewStandard(math32.NewColor("DarkBlue"))
+	mesh := graphic.NewMesh(geom, mat)
+	a.scene.Add(mesh)
 }
 
 func (a *App) RunApp() error {
