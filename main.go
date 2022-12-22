af5750f7dc25b83f4628df8d056cd01ba6547952
Jensen Holm <96762808+Jensen-holm@users.noreply.github.com>
2022-12-22 16:28:24 -0500

    got a blank app window with the main func

--- main.go
+++ main.go
@@ -1,4 +1,33 @@
 package main
 
-func main(){
+import (
+	"github.com/g3n/engine/app"
+	"github.com/g3n/engine/camera"
+	"github.com/g3n/engine/core"
+	"github.com/g3n/engine/gls"
+	"github.com/g3n/engine/renderer"
+	"time"
+)
+
+func main() {
+
+	// Create application and scene
+	a := app.App()
+	s := core.NewNode()
+
+	// Create perspective camera
+	cam := camera.New(1)
+	cam.SetPosition(0, 0, 3)
+	s.Add(cam)
+
+	// Set up perspective camera
+	// Run the application
+	a.Run(func(renderer *renderer.Renderer, deltaTime time.Duration) {
+		a.Gls().Clear(gls.DEPTH_BUFFER_BIT | gls.STENCIL_BUFFER_BIT | gls.COLOR_BUFFER_BIT)
+		err := renderer.Render(s, cam)
+		if err != nil {
+			panic(err)
+		}
+	})
+
 }
