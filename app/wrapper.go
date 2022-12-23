4cf1dc607ada1a37629eacd45423f056ed1bcd6e
Jensen Holm <96762808+Jensen-holm@users.noreply.github.com>
2022-12-22 20:04:53 -0500

    now its better

--- app/wrapper.go
+++ app/wrapper.go
@@ -66,16 +66,12 @@ func (w *Window) RunApp() error {
 	var err error = nil
 
 	w.app.Run(
-		func(
-			renderer *renderer.Renderer,
-			deltaTime time.Duration,
-		) {
-			w.app.
-				Gls().
-				Clear(gls.DEPTH_BUFFER_BIT |
+		func(renderer *renderer.Renderer, deltaTime time.Duration) {
+			w.app.Gls().Clear(
+				gls.DEPTH_BUFFER_BIT |
 					gls.STENCIL_BUFFER_BIT |
 					gls.COLOR_BUFFER_BIT,
-				)
+			)
 			err = renderer.Render(w.scene, w.cam)
 		},
 	)
