e33321548bc31afd9de11954cc0c412f22850630
Jensen Holm <96762808+Jensen-holm@users.noreply.github.com>
2022-12-23 18:44:51 -0500

    less complex way of adding big red ball

--- models/ball/ball.go
+++ models/ball/ball.go
@@ -1,13 +1,10 @@
 package ball
 
 import (
-	"github.com/Jensen-holm/RealTimeMLB/models"
 	"github.com/g3n/engine/geometry"
 	"github.com/g3n/engine/graphic"
 	"github.com/g3n/engine/material"
 	"github.com/g3n/engine/math32"
-	"github.com/g3n/engine/texture"
-	"log"
 )
 
 // Create a new baseball to get flown thorough the air
@@ -19,13 +16,9 @@ import (
 func NewBall() *graphic.Mesh {
 	s := geometry.NewSphere(10, 16, 16)
 
-	m, err := texture.NewTexture2DFromImage(models.DPath() + "ball/red.jpg")
-	if err != nil {
-		log.Fatalf("error loading materiel for ball from image: %s", err)
-	}
-
-	var mat = material.NewStandard(math32.NewColor("red"))
-	mat.AddTexture(m)
+	mat := material.NewStandard(
+		math32.NewColor("red"),
+	)
 
 	// create a mesh for the sphere
 	sphereMesh := graphic.NewMesh(s, mat)
