b386c37dd29d307629d710aa7be7634783eeb003
Jensen Holm <96762808+Jensen-holm@users.noreply.github.com>
2022-12-24 02:43:45 -0500

    good stopping point

--- win/run.go
+++ win/run.go
@@ -6,17 +6,22 @@ import (
 	"time"
 )
 
+func (w *Window) Rend(renderer *renderer.Renderer) error {
+	w.App.Gls().Clear(
+		gls.DEPTH_BUFFER_BIT |
+			gls.STENCIL_BUFFER_BIT |
+			gls.COLOR_BUFFER_BIT,
+	)
+	err := renderer.Render(w.Scene, w.Cam)
+	return err
+}
+
 func (w *Window) RunApp() error {
 	var err error = nil
 
 	w.App.Run(
 		func(renderer *renderer.Renderer, deltaTime time.Duration) {
-			w.App.Gls().Clear(
-				gls.DEPTH_BUFFER_BIT |
-					gls.STENCIL_BUFFER_BIT |
-					gls.COLOR_BUFFER_BIT,
-			)
-			err = renderer.Render(w.Scene, w.Cam)
+			err = w.Rend(renderer)
 		},
 	)
 
