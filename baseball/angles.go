5d56a17b60b7863cbd0f9b90b55ef2910acc1e53
Jensen Holm <96762808+Jensen-holm@users.noreply.github.com>
2023-01-20 10:28:55 -0500

    Cleaned up code, added more comments

--- baseball/angles.go
+++ baseball/angles.go
@@ -4,11 +4,7 @@ import (
 	"math"
 )
 
-// We need a function to somehow get the launch angle and the spray angle into a vector as well
-// to finish setting up the apply force function for each time a hit is scraped. It is kind of hard
-// to test anything while there are no games going on in the mlb right now.
-
-// CalculateVector -> Chat gpt gave me this function, excited to see if it will work
+// CalculateVector ->
 func CalculateVector(la, ev, spray float64) (float32, float32, float32) {
 
 	// convert from degrees to radians
